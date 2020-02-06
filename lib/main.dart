e68292c000e00746df12b71a3cfd498088250f93
jun108059 <youngjun108059@gmail.com>
2020-02-06 17:01:23 +0900

    [Update] Single-child layout widgets

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,10 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blue,
         body: SafeArea(
           child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.all(30.0),
+            padding: EdgeInsets.all(20.0),
             color: Colors.white,
             child: Text('Hello'), 
           ),
